eb802dee9559f20f7642e801f1fe873b099f3b8a
Charitha-SE <nadipannagaricharitha91@gmail.com>
2024-10-29 17:15:23 +0530

    Updated the shipment changes

---
+++ pages/Selectors/loginPage.js
@@ -0,0 +1,8 @@
+const LoginSelectors = {
+    username: "[name='email']",
+    password: "[name='password']",
+    loginButton: ".submit-button",
+    toaster: ".Toastify__toast-body div:nth-of-type(2)"
+};
+
+module.exports = { LoginSelectors };
---
+++ tests/driver.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test');
+const Driver = require('../pages/Actions/driver'); 
+const { login } = require('../helper/auth');
+
+test.beforeEach(async ({ page }) => {
+    await login(page);
+  });
+
+
+test('Verify the driver is created successfully', async ({ page }) => {
+    const driver = new Driver(page);
+
+    await driver.driverCreation();
+
+    const createdDriverName = driver.driverName;
+
+    const toastMessage = await page.textContent(driver.selectors.successToaster);
+    expect(toastMessage).toContain('New Driver Added Successfully');
+});
